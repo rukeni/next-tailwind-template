87f3a28869e5ca5b1b55fbd8a7f75e1ca3a0b6ed
양탕구리 <rukeni@naver.com>
2021-11-21 23:23:22 +0900

    Create Project By Next.js Starter example

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  semi: true,
+  useTabs: false,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  htmlWhitespaceSensitivity: 'css',
+};
