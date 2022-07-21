2018ecefb30010e3ec3bd01bee63c999c84b5007
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-07-21 23:35:56 +0900

    스토리북 설정 추가

---
+++ src/stories/Button.tsx
@@ -0,0 +1,53 @@
+import './button.css';
+
+interface ButtonProps {
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+const Button = ({
+  backgroundColor,
+  label,
+  primary = false,
+  size = 'medium',
+  ...props
+}: ButtonProps) => {
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
+  return (
+    <button
+      className={['storybook-button', `storybook-button--${size}`, mode].join(
+        ''
+      )}
+      style={{ backgroundColor }}
+      type="button"
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
---
+++ src/stories/Page.stories.tsx
@@ -0,0 +1,18 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import Page from './Page';
+
+export default {
+  component: Page,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+  title: 'Example/Page',
+} as ComponentMeta<typeof Page>;
+
+const Template: ComponentStory<typeof Page> = (args) => <Page {...args} />;
+
+export const LoggedOut = Template.bind({});
+
+export const LoggedIn = Template.bind({});
